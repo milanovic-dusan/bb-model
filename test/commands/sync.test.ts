9744737070c37e6594124a6e1c32e7ceb6ac58cb
Dusan Milanovic <dusan@backbase.com>
2019-11-29 14:06:49 +0100

    add unit tests

--- test/commands/sync.test.ts
+++ test/commands/sync.test.ts
@@ -1,11 +1,51 @@
-// tslint:disable-next-line:no-implicit-dependencies
-import { expect, test } from "@oclif/test";
+import fs = require("fs");
+import path = require("path");
+import { expect, test } from "../test";
+
+const PROMISE_DELAY = 50;
+
+const mockApi = api =>
+  api
+    .get(`/auth/login`)
+    .reply(200)
+    .get(`/portals/retail-banking-demo-wc3.json`)
+    .reply(200, _ =>
+      fs.createReadStream(path.join(__dirname, "..", "fixtures", "model.json"))
+    )
+    .post(`/auth/login`, { username: "admin", password: "admin" })
+    .reply(200);
+
+// TODO: mock fs
 
 describe("sync", () => {
   test
-    .stdout()
+    .nock(`http://localhost:8080/gateway/api`, mockApi)
     .command(["sync"])
-    .it("runs sync", ctx => {
-      expect(ctx.stdout).to.contain("");
+    // FIXME: properly handle async command run
+    .do(() => new Promise(resolve => setTimeout(resolve, PROMISE_DELAY)))
+    .it("creates a non-empty `app-model` file", ({}) => {
+      const appModel = fs.readFileSync("app-model.json", "utf8");
+      expect(appModel.length).to.be.greaterThan(0);
+    });
+
+  test
+    .nock(`http://localhost:8080/gateway/api`, mockApi)
+    .command(["sync"])
+    // FIXME: properly handle async command run
+    .do(() => new Promise(resolve => setTimeout(resolve, PROMISE_DELAY)))
+    .it("`app-model` does not contain preferences", ({}) => {
+      const appModel = fs.readFileSync("app-model.json", "utf8");
+
+      expect(appModel).not.to.contain("preferences");
+    });
+  test
+    .nock(`http://localhost:8080/gateway/api`, mockApi)
+    .command(["sync"])
+    // FIXME: properly handle async command run
+    .do(() => new Promise(resolve => setTimeout(resolve, PROMISE_DELAY)))
+    .it("`app-model` containes properties", ({}) => {
+      const appModel = fs.readFileSync("app-model.json", "utf8");
+
+      expect(appModel).to.contain("properties");
     });
 });
